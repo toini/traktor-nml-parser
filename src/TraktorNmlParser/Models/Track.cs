c2342bf01cb572df87a35682ce4cc55b47df0e2b
Toni Juvani <toni@tibber.com>
2025-04-12 16:39:14 +0300

    Parse track

--- src/TraktorNmlParser/Models/Track.cs
+++ src/TraktorNmlParser/Models/Track.cs
@@ -4,6 +4,17 @@ namespace TraktorNmlParser.Models;
 
 public class Track
 {
+    public string Path { get; set; } = string.Empty;
+    public string? Title { get; set; }
+    public string? Artist { get; set; }
+    public string? Comment { get; set; }
+    public float? Bpm { get; set; }
+    public string? Genre { get; set; }
+    public string? Key { get; set; }
+    public int? Bitrate { get; set; }
+    public int? Playtime { get; set; }
+    public List<CuePoint> CuePoints { get; set; } = new();
+
     public static Track FromXml(XElement entry)
     {
         var location = entry.Element("LOCATION");
@@ -18,15 +29,21 @@ public class Track
             .OrderBy(c => c.Start)
             .ToList();
 
+        var info = entry.Element("INFO");
+        var tempo = entry.Element("TEMPO");
+
         return new Track
         {
             Path = path,
-            Title = (string?)entry.Attribute("TITLE")!,
-            CuePoints = cues
+            Title = (string?)entry.Attribute("TITLE"),
+            Artist = (string?)entry.Attribute("ARTIST"),
+            CuePoints = cues,
+            Comment = (string?)info?.Attribute("COMMENT"),
+            Genre = (string?)info?.Attribute("GENRE"),
+            Key = (string?)info?.Attribute("KEY"),
+            Bitrate = (int?)info?.Attribute("BITRATE"),
+            Playtime = (int?)info?.Attribute("PLAYTIME"),
+            Bpm = (float?)tempo?.Attribute("BPM")
         };
     }
-
-    public string? Path { get; set; }
-    public string? Title { get; set; }
-    public List<CuePoint> CuePoints { get; set; } = new();
 }
